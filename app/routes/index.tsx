a33c2ef617289cb54f40cf243de2f264f5e2c306
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-11-23 07:44:21 +0530

    add instructions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,6 +14,26 @@ export default function Index() {
     <div className='remix__page'>
       <main>
         <h2>Welcome to Remix!</h2>
+        <hr />
+        <h3>Local development</h3>
+        <ol>
+          <li>
+            <code>vercel dev</code>
+          </li>
+          <li>
+            <code>npm run dev</code>
+          </li>
+        </ol>
+        <hr />
+        <h3>Deployment</h3>
+        <ol>
+          <li>
+            <code>npm run build</code>
+          </li>
+          <li>
+            <code>vercel --prod</code>
+          </li>
+        </ol>
       </main>
     </div>
   );
